1fd62afc138b6ba2d401b8416740c6e920bb13c8
Wong Jinfa <30624538+wongjinfa@users.noreply.github.com>
2022-11-16 11:59:31 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let Step_count = 0
 input.onButtonPressed(Button.A, function () {
     Step_count = 0
 })
@@ -8,6 +7,8 @@ input.onButtonPressed(Button.B, function () {
 input.onGesture(Gesture.Shake, function () {
     Step_count += 1
 })
+let Step_count = 0
+Step_count = 0
 basic.forever(function () {
     basic.showNumber(Step_count)
 })
